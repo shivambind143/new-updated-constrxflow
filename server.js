b044578b0309c6c5f239d0755b84e016813bf415
Shivam <shivambind9092@gmail.com>
2026-02-17 14:06:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(session({
-    secret: 'construxflow-secret-key-2024',
+    // secret: 'construxflow-secret-key-2024',
+    // the above one is for local host 
+    secret: process.env.SESSION_SECRET || 'defaultsecret',
+
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 24 * 60 * 60 * 1000 } // 24 hours
