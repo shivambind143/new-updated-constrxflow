35824c3c80b5f09990eb9ce2c68b21057cce40b8
Shivam <shivambind9092@gmail.com>
2026-02-17 14:26:30 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -25,12 +25,14 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,   // VERY IMPORTANT
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool.promise();
 
